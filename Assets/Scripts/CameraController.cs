a9be31903a7abcae5e0c3afea279f8d96321b2b0
Elijah Roussos <elijah.rou@gmail.com>
2017-04-23 16:26:26 +0200

    Created Pixel-Snap material for sprites Added CameraController Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+	public float speed;
+	private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = new Vector3(
+			target.transform.position.x,
+			target.transform.position.y,
+			transform.position.z
+			);
+		transform.position = Vector3.Lerp(
+			transform.position,
+			targetPos,
+			speed*Time.deltaTime
+		);
+	}
+}
