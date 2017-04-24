44c4a00992789a6b7ac1dc4d87a323ad7581b767
Elijah Roussos <elijah.rou@gmail.com>
2017-04-24 19:49:33 +0200

    Added laser bolts for player ships Added player 2 Animated lasers and players

---
+++ Assets/Scripts/LaserBolt.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBolt : MonoBehaviour {
+
+	public float speed;
+	public float decayRate;
+	public bool triggerDecay;
+	private Rigidbody2D rb;
+	public GameObject laserEffect;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		// obtain direction
+		rb.velocity = direction()*speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggerDecay){
+			decayRate -= Time.deltaTime;
+
+			if(decayRate < 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	void FixedUpdate(){
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Instantiate(laserEffect, transform.position, transform.rotation);
+		anim.SetTrigger("Impact");
+		rb.velocity = new Vector2(0f, 0f);
+		triggerDecay = true;
+	}
+
+	Vector3 direction(){
+		return Quaternion.AngleAxis(rb.rotation, Vector3.forward) * Vector3.up;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,103 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// parameters
+	public float boostForce;
+	public float boostLength;
+	public float boostCooldown;
+	public bool boostIsCooldown;
+	public bool boostActive;
+	private float counter;
 	public float velocity;
+	public float angular_velocity;
+
+	// gmae bodies
 	private Animator animator;
+	private Rigidbody2D rb;
+
+	// controls
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode thrust;
+	public KeyCode fire;
+	public KeyCode boost;
+
+	// laser
+	public GameObject laser;
+	public Transform shootPoint;
+	public float laserRate;
+	private float timer;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f){
-			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*velocity*Time.deltaTime, 0f, 0f));
+
+	void FixedUpdate(){	
+		if(boostIsCooldown){
+			if(boostActive){
+				counter += Time.fixedDeltaTime;
+				if(counter >= boostLength){
+					boostActive = false;
+					counter = 0;
+				}
+			}
+			else{
+				counter += Time.fixedDeltaTime;
+				if(counter >= boostCooldown){
+					boostIsCooldown = false;
+					counter = 0;
+				}
+			}
+	
 		}
-		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f){
-			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical")*velocity*Time.deltaTime, 0f));
+		// rotate left or right
+		if(Input.GetKey(left)){
+			 rb.MoveRotation(rb.rotation+angular_velocity);
+			 animator.SetTrigger("LeftTurn");
+		}
+		else if(Input.GetKey(right)){
+			 rb.MoveRotation(rb.rotation-angular_velocity);
+			 animator.SetTrigger("RightTurn");
+		}
+		else{
+			rb.MoveRotation(rb.rotation);
+		}
+		if(!boostActive){
+			if(Input.GetKey(thrust)){
+			// determine angle
+			//rb.AddForce(direction()*this.velocity*Time.fixedDeltaTime);
+			rb.velocity = direction()*this.velocity;
+			}
+			else{
+				rb.velocity = new Vector2(0f, 0f);
+			}
 		}
 		
-		animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-		animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+		if(Input.GetKey(boost) && boostActive == false && boostIsCooldown == false){
+			boostActive = true;
+			boostIsCooldown = true;
+			rb.velocity = direction();
+			rb.AddForce(direction()*boostForce*Time.fixedDeltaTime);
+		}
 	}
 	
+	void Update(){
+		//animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+		//animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+		timer += Time.deltaTime;
+		if(Input.GetKey(fire)){
+			if(timer > 1/laserRate){
+				Instantiate(laser, shootPoint.position, shootPoint.rotation);
+				timer = 0;
+			}
+		}
+	}
+
+	Vector3 direction(){
+		return Quaternion.AngleAxis(rb.rotation, Vector3.forward) * Vector3.up;
+	}
 }
